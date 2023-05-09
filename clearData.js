526cc663b69e644811ed189306a050817bc53be6
Javier Ramos <54128454+javierrh2812@users.noreply.github.com>
2023-05-09 14:42:54 -0500

    Update clearData.js

--- clearData.js
+++ clearData.js
@@ -1,22 +1,40 @@
 import fs from "fs";
 import csv from "fast-csv";
 
-const entries = []
-const lang = 'es'
+const entries = [];
+const lang = "es";
+
+
+const contentType = process.argv[2];
+
+if (!contentType) {
+  throw Error('content type required ar argument')
+}
 
 const originPath = "./inputs/data.csv";
-const targetPath = './inputs/data.json'
+const targetPath = "./inputs/data.json";
 
 fs.createReadStream(originPath)
   .pipe(csv.parse({ headers: true }))
   .on("error", (error) => console.error(error))
   .on("data", (row) => {
-    Object.keys(row).forEach(key=> {
-      row[key] = {[lang]: row[key]} 
-    })
-    entries.push( {fields: row} )
+    Object.keys(row).forEach((key) => {
+      row[key] = { [lang]: row[key] };
+    });
+    entries.push({
+      fields: row,
+      sys: {
+        contentType: {
+          sys: {
+            type: "Link",
+            linkType: "ContentType",
+            id: contentType,
+          },
+        },
+      },
+    });
   })
   .on("end", () => {
-    fs.writeFileSync(targetPath, JSON.stringify({entries},null,2))
-    console.log('success!')
+    fs.writeFileSync(targetPath, JSON.stringify({ entries }, null, 2));
+    console.log("success!");
   });
