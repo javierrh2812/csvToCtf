326f0099667d8d4165eb529dfcbb333efa1cc071
Javier Ramos <54128454+javierrh2812@users.noreply.github.com>
2023-05-09 19:30:04 +0000

    wip

---
+++ clearData.js
@@ -0,0 +1,22 @@
+import fs from "fs";
+import csv from "fast-csv";
+
+const entries = []
+const lang = 'es'
+
+const originPath = "./inputs/data.csv";
+const targetPath = './inputs/data.json'
+
+fs.createReadStream(originPath)
+  .pipe(csv.parse({ headers: true }))
+  .on("error", (error) => console.error(error))
+  .on("data", (row) => {
+    Object.keys(row).forEach(key=> {
+      row[key] = {[lang]: row[key]} 
+    })
+    entries.push( {fields: row} )
+  })
+  .on("end", () => {
+    fs.writeFileSync(targetPath, JSON.stringify({entries},null,2))
+    console.log('success!')
+  });
